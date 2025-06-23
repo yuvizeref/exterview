cf1dcd18c01afa22b67d9f1275d29389dbeefccb
Yuvraj <yuvizeref@gmail.com>
2025-06-23 17:11:12 +0000

    Configure CORS, Persist data.

--- src/main/java/com/codewithuv/exterview/configurations/WebConfig.java
+++ src/main/java/com/codewithuv/exterview/configurations/WebConfig.java
@@ -2,15 +2,17 @@ package com.codewithuv.exterview.configurations;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
-import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
-@EnableWebMvc
 public class WebConfig implements WebMvcConfigurer {
 
     @Override
     public void addCorsMappings(@SuppressWarnings("null") CorsRegistry registry) {
-        registry.addMapping("/**").allowedOrigins("http://localhost:5173");
+        registry.addMapping("/**")
+                .allowedOrigins("http://localhost:5173")
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                .allowedHeaders("Content-Type", "Authorization")
+                .allowCredentials(true);
     }
 }
